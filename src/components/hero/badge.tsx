22689937c2150fdd052206e06527ffcd7a922315
Felipe Carvalho <felipe_pc7@hotmail.com>
2025-02-12 19:07:51 -0300

    create badge component

---
+++ src/components/hero/badge.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx"
+
+interface BadgeProps {
+    theme: 'light' | 'dark',
+    text: string
+}
+
+export function Badge({ theme, text }: BadgeProps) {
+    return <span className={clsx(
+        "text-sm py-[10px] px-4 rounded-full", 
+        {
+            'bg-lagoon text-white': theme === 'dark',
+            'bg-onahau text-black': theme === 'light'
+        })
+    }>
+        { text }
+    </span>
+}
